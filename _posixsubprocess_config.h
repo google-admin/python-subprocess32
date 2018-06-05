bb19d0f29d2d8ffb3e9c026485237d48e8bc1a57
Gregory P. Smith [Google] <greg@krypto.org>
2018-06-04 18:05:31 -0700

    Fix restore_signals=True to not be a no-op!
    It wasn't being compiled as anything useful due to a missing
    include of signal.h.  This also adds the missing functional test
    that would have caught this.  Needs pushing to upstream CPython
    as well.

--- _posixsubprocess_config.h
+++ _posixsubprocess_config.h
@@ -1,112 +1,113 @@
+/* _posixsubprocess_config.h.  Generated from _posixsubprocess_config.h.in by configure.  */
 /* _posixsubprocess_config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'.
    */
-#undef HAVE_DIRENT_H
+#define HAVE_DIRENT_H 1
 
 /* Define if you have the 'dirfd' function or macro. */
-#undef HAVE_DIRFD
+#define HAVE_DIRFD 1
 
 /* Define to 1 if you have the <fcntl.h> header file. */
-#undef HAVE_FCNTL_H
+#define HAVE_FCNTL_H 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#undef HAVE_INTTYPES_H
+#define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the <memory.h> header file. */
-#undef HAVE_MEMORY_H
+#define HAVE_MEMORY_H 1
 
 /* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
-#undef HAVE_NDIR_H
+/* #undef HAVE_NDIR_H */
 
 /* Define to 1 if you have the `pipe2' function. */
-#undef HAVE_PIPE2
+#define HAVE_PIPE2 1
 
 /* Define to 1 if you have the `setsid' function. */
-#undef HAVE_SETSID
+#define HAVE_SETSID 1
 
 /* Define to 1 if you have the <signal.h> header file. */
-#undef HAVE_SIGNAL_H
+#define HAVE_SIGNAL_H 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#undef HAVE_STDINT_H
+#define HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#undef HAVE_STDLIB_H
+#define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
+#define HAVE_STRINGS_H 1
 
 /* Define to 1 if you have the <string.h> header file. */
-#undef HAVE_STRING_H
+#define HAVE_STRING_H 1
 
 /* Define to 1 if you have the <sys/cdefs.h> header file. */
-#undef HAVE_SYS_CDEFS_H
+#define HAVE_SYS_CDEFS_H 1
 
 /* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'.
    */
-#undef HAVE_SYS_DIR_H
+/* #undef HAVE_SYS_DIR_H */
 
 /* Define to 1 if you have the <sys/ndir.h> header file, and it defines `DIR'.
    */
-#undef HAVE_SYS_NDIR_H
+/* #undef HAVE_SYS_NDIR_H */
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#undef HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/syscall.h> header file. */
-#undef HAVE_SYS_SYSCALL_H
+#define HAVE_SYS_SYSCALL_H 1
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#undef HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
+#define HAVE_UNISTD_H 1
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT "https://github.com/google/python-subprocess32/"
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+#define PACKAGE_NAME "_posixsubprocess32"
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING "_posixsubprocess32 3.5"
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
+#define PACKAGE_TARNAME "_posixsubprocess32"
 
 /* Define to the home page for this package. */
-#undef PACKAGE_URL
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
+#define PACKAGE_VERSION "3.5"
 
 /* Define to 1 if you have the ANSI C header files. */
-#undef STDC_HEADERS
+#define STDC_HEADERS 1
 
 /* Define on OpenBSD to activate all library features */
-#undef _BSD_SOURCE
+/* #undef _BSD_SOURCE */
 
 /* Define on Irix to enable u_int */
-#undef _BSD_TYPES
+#define _BSD_TYPES 1
 
 /* Define on Darwin to activate all library features */
-#undef _DARWIN_C_SOURCE
+#define _DARWIN_C_SOURCE 1
 
 /* Define on Linux to activate all library features */
-#undef _GNU_SOURCE
+#define _GNU_SOURCE 1
 
 /* Define on NetBSD to activate all library features */
-#undef _NETBSD_SOURCE
+#define _NETBSD_SOURCE 1
 
 /* Define to activate features from IEEE Stds 1003.1-2008 */
-#undef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200809L
 
 /* Define to the level of X/Open that your system supports */
-#undef _XOPEN_SOURCE
+#define _XOPEN_SOURCE 700
 
 /* Define to activate Unix95-and-earlier features */
-#undef _XOPEN_SOURCE_EXTENDED
+#define _XOPEN_SOURCE_EXTENDED 1
 
 /* Define on FreeBSD to activate all library features */
-#undef __BSD_VISIBLE
+#define __BSD_VISIBLE 1
